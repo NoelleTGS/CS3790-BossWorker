0c3d5d9901419f81336e4775ad2209e9a2e43f39
Noelle Zahara <noellezahara@gmail.com>
2024-10-09 15:10:07 -0600

    Display usage for incorrect arg count in worker

--- worker.cpp
+++ worker.cpp
@@ -4,6 +4,10 @@
 using namespace std;
 
 int main(const int argc, char *argv[]) {
+    if (argc != 3) {
+        cout << "Usage: " << argv[0] << " <start> <end>" << endl;
+        return 1;
+    }
 
 
 }
