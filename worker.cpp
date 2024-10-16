d1bfcfae5d21cf203466ba57ea471db866c6c516
Noelle Zahara <noellezahara@gmail.com>
2024-10-16 14:51:51 -0600

    Return false when checking if 1 or less is prime

--- worker.cpp
+++ worker.cpp
@@ -4,6 +4,7 @@
 using namespace std;
 
 bool isPrime(int number) {
+    if (number <= 1) return false;
     for (int i = 2; i < number; i++) {
         if (number % i == 0) {
             return false;
