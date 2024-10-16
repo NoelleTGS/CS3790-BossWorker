998be8a983733052586e0f8293bb279f60e81db8
Noelle Zahara <noellezahara@gmail.com>
2024-10-16 15:17:06 -0600

    Ensure proper number of arguments for boss

--- boss.cpp
+++ boss.cpp
@@ -1,5 +1,11 @@
+#include <iostream>
 using namespace std;
 
-int main() {
+int main(const int argc, char *argv[]) {
+    if (argc != 3) {
+        cout << "Usage: " << argv[0] << " <no of workers> <end>" << endl;
+        return 1;
+    }
+
     return 0;
 }
