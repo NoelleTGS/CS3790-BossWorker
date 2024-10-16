1c97f16ae46d4e0285fa7e0b490b91c4cff4ad64
Noelle Zahara <noellezahara@gmail.com>
2024-10-16 14:50:57 -0600

    Create function for determining if number is prime

--- worker.cpp
+++ worker.cpp
@@ -3,6 +3,15 @@
 #include <string>
 using namespace std;
 
+bool isPrime(int number) {
+    for (int i = 2; i < number; i++) {
+        if (number % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
 int main(const int argc, char *argv[]) {
     if (argc != 3) {
         cout << "Usage: " << argv[0] << " <start> <end>" << endl;
