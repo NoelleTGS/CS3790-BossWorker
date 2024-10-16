3d685b6b567a44e2c04b8da0bf0f5d9bf501ddf4
Noelle Zahara <noellezahara@gmail.com>
2024-10-16 16:06:36 -0600

    Ensure remainder is added to final execl for uneven divisions

--- boss.cpp
+++ boss.cpp
@@ -14,9 +14,12 @@ int main(const int argc, char *argv[]) {
 
     int status;
     for (int i = 0; i < stoi(argv[1]); i++) {
+        int endRange = (i+1)*range;
+        if (i == stoi(argv[1]) - 1) endRange = stoi(argv[2]);
+
         int pid = fork();
         if (pid == 0) {
-            execl("./worker", "./worker", to_string((i*range)+1).c_str(), to_string((i+1)*range).c_str(), (char *) NULL);
+            execl("./worker", "./worker", to_string((i*range)+1).c_str(), to_string(endRange).c_str(), (char *) NULL);
         } else {
             wait(&status);
             printf("child returned status code : %d\n", status);
